52a3f0afd4fe1af4174a367f4c9a7c23f8d60e79
pushpendrarawat672 <124857937+pushpendrarawat672@users.noreply.github.com>
2024-10-23 22:23:11 +0530

    install dependencies

---
+++ user/app.js
@@ -0,0 +1,9 @@
+// const express = require('express')
+
+// const dotenv = require('dotenv')
+
+import * as express  from "express";
+import * as dotenv from "dotenv";
+dotenv.config()
+
+console.log(process.env.PORT)
